9379e7e0d7b813d22caae5b7cf6bfcd13e5e1ae4
Lim Jitt Hing <jhlim.2023@smu.edu.sg>
2024-06-18 16:08:36 +0800

    Configure backend API to fetch all model types from db

---
+++ backend/controllers/modelController.js
@@ -0,0 +1,27 @@
+const Models = require("../models/modelModel");
+
+const getAllModels = async (req, res) => {
+  const cursor = await Models.find({});
+  //   const dbModels = await cursor.toArray();
+  if (cursor === null) {
+    return res.status(400).json({ message: "No Models Found" });
+  }
+  return res.status(200).json(cursor);
+};
+
+const create_model = async (req, res) => {
+  const newModel = req.body;
+
+  const foundModel = await Models.findOne({ modelName: newModel.name });
+
+  if (foundModel !== null) {
+    return res.status(400).json({ message: "Model already exists" });
+  }
+
+  const createdModel = await Models.create({
+    modelName: newModel.name,
+  });
+
+  return res.status(200).json({ message: "Model created succesfully" });
+};
+module.exports = { getAllModels, create_model };
