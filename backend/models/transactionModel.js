91194c8226bd31075f7dd5d24e00c35ccb723986
DwightCJH <dwightcjh@gmail.com>
2024-07-03 17:16:37 +0800

    removed "goal" section in transactions/new record

--- backend/models/transactionModel.js
+++ backend/models/transactionModel.js
@@ -4,7 +4,7 @@ const transactionSchema = new mongoose.Schema({
   username: { type: String, required: true },
   title: {type: String, required: true },
   category: { type: String, required: true },
-  goal: { type: String, required: true },
+  // goal: { type: String, required: true },
   amount: { type: Number, required: true },
   date: { type: Date, required: true, default: Date.now },
 });
