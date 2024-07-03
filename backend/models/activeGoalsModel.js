bbe1c224be0212ebe020b011d79ec04aedf53d4d
Lim Jitt Hing <jhlim.2023@smu.edu.sg>
2024-07-03 16:56:34 +0800

    Update DB and Add New Owned Pigs table

--- backend/models/activeGoalsModel.js
+++ backend/models/activeGoalsModel.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const activeGoalsSchema = new mongoose.Schema({
-  username: String,
-  title: String,
-  price: String,
-  startDate: String,
-  saved: mongoose.Schema.Types.Decimal128,
+  username: { type: String, required: true },
+  title: { type: String, required: true },
+  price: { type: String, required: true },
+  startDate: { type: String, required: true },
+  saved: { type: Number, required: true },
+  status: { type: String, default: "In Progress" },
 });
 
 // to write one to update amount saved per goal
